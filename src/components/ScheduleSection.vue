437b33efc4ae8c578c32e3f6dd6b0eee01fe306b
Jenny Heang <86590058+Seavleu@users.noreply.github.com>
2024-12-10 10:55:46 +0900

    feat: update img

--- src/components/ScheduleSection.vue
+++ src/components/ScheduleSection.vue
@@ -4,7 +4,7 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "ScheduleSection",
   setup() {
-    const icons = ["ico_1.png", "ico_2.png", "ico_3.png", "ico_4.png", "ico_5.png", "ico_6.png"];
+    const icons = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
 
     const getIconPath = (icon: string) => {
       return new URL(`../assets/images/${icon}`, import.meta.url).href;
@@ -36,8 +36,8 @@ export default defineComponent({
 .schedule-section {
   padding: 60px 15px;  
   text-align: center;
-  // background: url('@/assets/images/gallery3.jpg') no-repeat center center fixed;
-  // background-size: cover;
+  background: url('@/assets/images/gallery3.jpg') no-repeat center center fixed;
+  background-size: cover;
 
   .schedule-title {
     font-size: 32px;
